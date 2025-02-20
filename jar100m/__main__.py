126a2279331bb8f20f8db0b53ff4df2be36d64b5
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-02-20 11:36:58 -0700

    Basic character predictor model

--- jar100m/__main__.py
+++ jar100m/__main__.py
@@ -1,14 +1,15 @@
-import random as rand
 from jax import grad
 import jax.numpy as np
 from jax.example_libraries import optimizers
+from jax.nn import softmax
+import jax.random
 
 from jar100m.dataset import Dataset
 from jar100m.functions import relu, mse
 from jar100m.layers import *
 
 TRAIN_SPLIT = 0.9
-CONTEXT_WINDOW_SIZE = 64
+CONTEXT_WINDOW_SIZE = 1
 EPOCHS = 10
 LEARNING_RATE = 0.01
 
@@ -19,11 +20,22 @@ spliceIndex = int(len(shakespeare) * TRAIN_SPLIT)
 train = shakespeare[:spliceIndex]
 validate = shakespeare[spliceIndex:]
 
-dataset = Dataset(shakespeare, CONTEXT_WINDOW_SIZE)
+dataset = Dataset(shakespeare[:1000], CONTEXT_WINDOW_SIZE)
+rng = jax.random.key(0)
 
 def model(params, x):
-    for weights, biases in params:
-        x = relu(np.dot(x, weights) + biases)
+    logits = params[x[-1]]
+    return logits
+
+def generate(params, x: np.ndarray, n):
+    global rng
+
+    for _ in range(n):
+        logits = model(params, x)
+        #probs = softmax(logits)
+        next_idx = jax.random.categorical(rng, logits, shape=(1,))[0]
+        rng, _ = jax.random.split(rng)
+        x = np.concatenate((x, np.array([next_idx])))
 
     return x
 
@@ -31,10 +43,10 @@ def loss_fn(params, inp, expected_outp):
     predictions = model(params, inp)
     return mse(predictions, expected_outp)
 
-params = [
-    fully_connected(2, 2),
-    fully_connected(2, 1),
-]
+params = embedding(len(dataset.vocab), len(dataset.vocab))
+
+tokens = generate(params, dataset.encode("hi"), 20)
+print(dataset.decode(tokens))
 
 adam_init, adam_update, get_params = optimizers.adam(LEARNING_RATE)
 optimizer_state = adam_init(params)
@@ -49,5 +61,9 @@ for _ in range(EPOCHS):
         params = get_params(optimizer_state)
         
         loss = loss_fn(params, inp, expected_outp)
+
     total_loss += loss
     print(total_loss/len(dataset))
+
+new_tokens = generate(params, dataset.encode("\n"), 20)
+print(dataset.decode(new_tokens))
