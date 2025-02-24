adc3bf21b97c7d2046be9d762787c610007d3bcb
Josh Arbon <jarbon@student.neumont.edu>
2025-02-24 11:38:38 -0700

    refactor dataset

--- jar100m/dataset.py
+++ jar100m/dataset.py
@@ -1,10 +1,12 @@
 from typing import List, Tuple
+from jax import random
+import random as rand
 import jax.numpy as np
 
 class Dataset:
-    def __init__(self, text) -> None:
+    def __init__(self, text, context_size) -> None:
         self.id_char_map = list(set(text))
-        self.char_id_map = { char: id for id, char in enumerate(self.id_char_map) }
+        self.char_id_map = self.randomVecsInVocab(context_size)
 
         self.text = self.encode(text)
         
@@ -18,6 +20,19 @@ class Dataset:
 
         self.inputs = np.array(inputs)
         self.outputs = np.array(outputs)
+    
+    def getRandomContextWindow(self, context_size: int):
+        random_position = rand.randint(0, len(self.text) - 1)
+        return np.array([self.text[i] for i in range(random_position, random_position + context_size)])
+
+    def randomVecsInVocab(self, context_size: int):
+        key = random.PRNGKey(0)
+        keys = random.split(key, len(self.id_char_map))
+        randomized_vocab_vectors = {
+            char: random.uniform(keys[i], shape=(1, context_size), minval=-1.0, maxval=1.0)
+            for i, char in enumerate(self.id_char_map)
+        }
+        return randomized_vocab_vectors
 
     def encode(self, text: str) -> List[int]:
         return np.array([self.char_id_map[char] for char in text])
