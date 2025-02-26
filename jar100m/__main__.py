eb24c601f8e8803df6d639acb434c9efa2e5fc9e
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-02-26 11:54:36 -0700

    Begin switch to pytorch

--- jar100m/__main__.py
+++ jar100m/__main__.py
@@ -1,16 +1,19 @@
-from jax import grad
-import jax.numpy as np
-from jax.example_libraries import optimizers
-import jax.random
-
-from jar100m.dataset import Dataset
-from jar100m.functions import *
-from jar100m.layers import *
+import torch
+from torch import nn
+from torch.optim import Adam
 
 TRAIN_SPLIT = 0.9
 CONTEXT_WINDOW_SIZE = 1
 EPOCHS = 10
 LEARNING_RATE = 0.01
+
+class Model(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.linear = nn.Linear(2, 1)
+
+    def forward(self, x):
+        return self.linear(x)
 
 with open("dataset.txt", 'r') as file:
     shakespeare = file.read()
@@ -19,50 +22,42 @@ spliceIndex = int(len(shakespeare) * TRAIN_SPLIT)
 train = shakespeare[:spliceIndex]
 validate = shakespeare[spliceIndex:]
 
+loss_fn = nn.MSELoss()
+model = Model()
+optimizer = Adam(model.parameters(), lr=0.1)
+
+"""
 dataset = Dataset(shakespeare[:1000], CONTEXT_WINDOW_SIZE)
 rng = jax.random.key(0)
+"""
 
-def model(params, x):
-    logits = params[x[-1]]
-    return logits
-
-def generate(params, x: np.ndarray, n):
-    global rng
-
-    for _ in range(n):
-        logits = model(params, x)
-        #probs = softmax(logits)
-        next_idx = jax.random.categorical(rng, logits, shape=(1,))[0]
-        rng, _ = jax.random.split(rng)
-        x = np.concatenate((x, np.array([next_idx])))
-
-    return x
-
-def loss_fn(params, inp, expected_outp):
-    logits = model(params, inp)
-    return cross_entropy_loss(logits, expected_outp)
-
-params = embedding(len(dataset.vocab), len(dataset.vocab))
-
-tokens = generate(params, dataset.encode("hi"), 20)
-print(dataset.decode(tokens))
-
-adam_init, adam_update, get_params = optimizers.adam(LEARNING_RATE)
-optimizer_state = adam_init(params)
+inputs = []
+outputs = []
+for i in range(100):
+    for j in range(100):
+        inputs.append(torch.tensor([i/100, j/100]))
+        outputs.append(torch.tensor([(i/100 + j/100)/2]))
 
 for _ in range(EPOCHS):
     total_loss = 0
-    for i in range(len(dataset)):
-        inp, expected_outp = dataset[i]
+    for i in range(len(inputs)):
+        #inp, expected_outp = dataset[i]
+        inp = inputs[i]
+        pred = model(inp)
 
-        grads = grad(loss_fn)(params, inp, expected_outp)
-        optimizer_state = adam_update(0, grads, optimizer_state)
-        params = get_params(optimizer_state)
-        
-        loss = loss_fn(params, inp, expected_outp)
+        loss = loss_fn(pred, outputs[i])
+        total_loss += loss.item()
+
+        model.zero_grad()
+        loss.backward()
+        optimizer.step()
 
     total_loss += loss
-    print(total_loss/len(dataset))
+    print(total_loss/len(inputs))
 
-new_tokens = generate(params, dataset.encode("\n"), 20)
-print(dataset.decode(new_tokens))
+with torch.no_grad():
+    a = float(input())
+    b = float(input())
+    inp = torch.tensor([a, b])
+    out = model(inp)
+    print(out)
