eb581c2271e2976d43db7e8b4f19bd1c09e1a707
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-02-27 06:05:25 -0700

    Embed token position

--- jar100m/__main__.py
+++ jar100m/__main__.py
@@ -9,14 +9,23 @@ from jar100m.device import device
 TRAIN_SPLIT = 0.9
 CONTEXT_WINDOW_SIZE = 3
 EPOCHS = 10
+EMBED_DIMENSIONS = 8
 
 class Model(nn.Module):
-    def __init__(self, vocab_len: int) -> None:
+    def __init__(self, vocab_len: int, context_window_len: int) -> None:
         super().__init__()
-        self.embedding = nn.Embedding(vocab_len, vocab_len)
+        self.context_window_len = context_window_len
+
+        self.info_embedding = nn.Embedding(vocab_len, EMBED_DIMENSIONS)
+        self.position_embedding = nn.Embedding(context_window_len, EMBED_DIMENSIONS)
+        self.unembed = nn.Linear(EMBED_DIMENSIONS, vocab_len)
 
     def forward(self, x):
-        logits = self.embedding(x)
+        num_toks = x.shape[0]
+
+        embedding = self.info_embedding(x[-self.context_window_len:num_toks])
+        position_embeddings = self.position_embedding(torch.arange(min(num_toks, self.context_window_len)))
+        logits = self.unembed(embedding + position_embeddings)
         return logits
 
 with open("dataset.txt", 'r') as file:
@@ -28,7 +37,7 @@ validate = shakespeare[spliceIndex:]
 
 dataset = Dataset(shakespeare[:50000], CONTEXT_WINDOW_SIZE)
 
-model = Model(len(dataset.vocab)).to(device)
+model = Model(len(dataset.vocab), CONTEXT_WINDOW_SIZE).to(device)
 optimizer = Adam(model.parameters(), lr=0.01)
 
 for _ in range(EPOCHS):
@@ -48,7 +57,7 @@ for _ in range(EPOCHS):
 
 def generate(sequence, n):
     for _ in range(n):
-        logits = model(sequence[-1])
+        logits = model(sequence)[-1]
         probs = F.softmax(logits, dim=0)
         next = torch.multinomial(probs, num_samples=1)
         sequence = torch.cat((sequence, next))
