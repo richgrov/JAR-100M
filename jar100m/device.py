46f1a895aa094551c1be5c77e5f4a01c9c35b826
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-03-10 11:35:48 -0600

    Remove unused file

--- jar100m/device.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    print("Running on cuda")
-    device = torch.device("cuda")
-else:
-    print("Running on cpu")
-    device = torch.device("cpu")
-
